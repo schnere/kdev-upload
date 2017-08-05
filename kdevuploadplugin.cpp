b58d45f6078541c7f961db947b54d9757e62be7c
Reinhard Schneidewind <schnere@bveml.net>
2017-08-05 09:40:14 +0200

    changed back UploadPlugin::contextMenuExtension for kdevplatform > 5.1.1

--- kdevuploadplugin.cpp
+++ kdevuploadplugin.cpp
@@ -214,12 +214,12 @@ KDevelop::ContextMenuExtension UploadPlugin::contextMenuExtension(KDevelop::Cont
                 UploadProfileModel* model = m_projectProfileModels.value(project);
                 if (model && model->rowCount()) {
                     QAction *action;
-                    action = new QAction(i18n("Upload..."), this);
+                    action = new QAction(i18n("Upload..."), parent);
                     action->setIcon(QIcon::fromTheme("go-up"));
                     connect(action, SIGNAL(triggered()), this, SLOT(upload()));
                     cmExtension.addAction(KDevelop::ContextMenuExtension::FileGroup, action);
     
-                    action = new QAction(i18n("Quick Upload"), this);
+                    action = new QAction(i18n("Quick Upload"), parent);
                     action->setIcon(QIcon::fromTheme("go-up"));
                     connect(action, SIGNAL(triggered()), this, SLOT(quickUpload()));
                     cmExtension.addAction(KDevelop::ContextMenuExtension::FileGroup, action);
@@ -229,7 +229,7 @@ KDevelop::ContextMenuExtension UploadPlugin::contextMenuExtension(KDevelop::Cont
             }
         }
     }
-    return KDevelop::IPlugin::contextMenuExtension(context);
+    return KDevelop::IPlugin::contextMenuExtension(context, parent);
 }
 
 void UploadPlugin::upload()
